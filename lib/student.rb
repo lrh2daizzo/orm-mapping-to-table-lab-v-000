127fa919e91cdb10b36eaf97676d61b4d407a01e
Luis Herrera <herrera.lrh@gmail.com>
2020-10-31 01:33:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,4 +7,17 @@ class Student
     @grade = grade
     @id = id
   end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      );
+      SQL
+
+      DB[:conn].execute(SQL)
+    end
+    
 end
